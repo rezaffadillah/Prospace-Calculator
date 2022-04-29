b5259de40429b86c30bd70dc437abbb3d2867fc6
rezaffadillah <104638599+rezaffadillah@users.noreply.github.com>
2022-04-29 22:12:00 +0700

    Update PageFactoryManager.java

--- src/main/java/pagesfactory/PageFactoryManager.java
+++ src/main/java/pagesfactory/PageFactoryManager.java
@@ -3,16 +3,10 @@ package pagesfactory;
 import context.TestContext;
 import pagesfactory.Calculator.CalculatorPageAction;
 import pagesfactory.Calculator.CalculatorPageValidation;
-import pagesfactory.products.ProductsPageValidation;
 
 public class PageFactoryManager {
     private static CalculatorPageAction calculatorPageAction;
     private static CalculatorPageValidation calculatorPageValidation;
-    private static ProductsPageValidation productsPageValidation;
-
-    public static ProductsPageValidation getProductsPageValidation(TestContext context) {
-        return productsPageValidation == null ? new ProductsPageValidation(context) : productsPageValidation;
-    }
 
     public static CalculatorPageAction getCalculatorPageAction(TestContext context) {
         return calculatorPageAction == null ? new CalculatorPageAction(context) : calculatorPageAction;
